9f975a209e617685764618d299ed3ce9f4780d60
Monica Cherrez Solis <78126056+MoCheSo@users.noreply.github.com>
2022-05-02 18:49:00 -0500

    Delete 9.RMI/RMI directory

--- 9.RMI/RMI/DemoServerMT/src/libro/cap07/rmi/ObjetoRemoto.java
+++
@@ -1,8 +0,0 @@
-package libro.cap07.rmi;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ObjetoRemoto extends Remote {
-
-    public String obtenerSaludo(String nombre) throws RemoteException;
-}
